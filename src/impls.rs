c09c8ba68ce43b6dc701c16ca5aeea16cabc25ba
Sach97 <sacha.arbonel@hotmail.fr>
2019-11-05 10:42:28 +0100

    test on add

--- src/impls.rs
+++ src/impls.rs
@@ -24,7 +24,7 @@ fn build_func_wrap(func: &syn::ItemFn) -> quote::__rt::TokenStream {
 
     let wrap_name = quote::format_ident!("rust_{}", name);
     let wrapped = quote! {
-    #[no_mangle]
+    [no_mangle]
     pub extern "C" fn #wrap_name(#inputs) #output {
         #name(
             #(#extracted_inputs,)*
@@ -37,4 +37,24 @@ fn build_func_wrap(func: &syn::ItemFn) -> quote::__rt::TokenStream {
 fn build_func_original(func: &syn::ItemFn) -> quote::__rt::TokenStream {
     let original = quote! { #func };
     original
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    use crate::quote::ToTokens;
+
+    #[test]
+    fn test_build_func_original() {
+        let source = quote! {  fn add(a: i32, b: i32) -> i32 {
+             a + b
+        }};
+        let expected = quote! {[ no_mangle ] pub extern "C" fn rust_add ( a : i32 , b : i32 ) -> i32 { add ( a , b , ) }};
+        let input = syn::parse_quote::parse(source);
+        assert_eq!(
+            build_func_wrap(&input).to_string(),
+            quote! {#expected}.to_string()
+        );
+    }
 }
